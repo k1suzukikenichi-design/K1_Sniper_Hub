ce7cbda3fce0feb4878fe78da68f37965b249ae7
K1 <k1.ai.investment.lab@gmail.com>
2026-02-23 17:05:27 +0900

    Initial Commit

---
+++ K1_Sniper_Hub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  K1_Sniper_Hub
+//
+//  Created by 鈴木健一 on 2026/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ K1_Sniper_Hub/K1_Sniper_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  K1_Sniper_HubApp.swift
+//  K1_Sniper_Hub
+//
+//  Created by 鈴木健一 on 2026/02/23.
+//
+
+import SwiftUI
+
+@main
+struct K1_Sniper_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
